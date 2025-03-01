bb319a93fc99874ba182bbaa069d9888e70a978b
gray reyes <39613924-gvr1040@users.noreply.replit.com>
2025-03-01 22:20:05 +0000

    updating - php

---
+++ Get the facts/Quiz.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Our Service</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Quiz</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="send_sms.php">Test Center Finder</a></li>
+                <li><a href="Learn.php">Get the Facts</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <h2></a>Question 1:</h2>
+        <p> </p>
+    </section>
+    <section>
+        <h2>Question 2:</h2>
+    </section>
+    <footer>
+        <p>&copy; 2025 STD Notification Service - Aware Link. All rights reserved.</p>
+    </footer>
+</body>
+</html>
